039e4f251c7f45d135f9d025264868ef824f6980
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-19 15:30:09 -0500

    add components to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios'
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Quotes from "./components/Quotes";
+import News from "./components/News";
+import StudyGroup from "./components/StudyGroup";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/quotes" component={Quotes} />
+        <Route exact path="/news" component={News} />
+        <Route exact path="/study-group" component={StudyGroup} />
+      </Switch>
     </div>
   );
 }
