bc6ae5f1b11a4af1a5993bb25f2e20327cd62a2e
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-20 11:44:07 -0500

    style quote and delete number of votes

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -5,13 +5,13 @@ export default class Quote extends Component {
   render() {
     return (
       <div className="container">
-        <blockquote className="blockquote">
+        <blockquote className="blockquote text-light bg-dark">
           <p className="mb-0">{this.props.quote.en}</p>
           <footer className="blockquote-footer">
             {this.props.quote.author}
           </footer>
           <Rating>{this.props.quote.rating}</Rating>
-          <span>voted by {this.props.quote.numberOfVotes} people</span>
+          {/* <span>voted by {this.props.quote.numberOfVotes} people</span> */}
         </blockquote>
       </div>
     );
