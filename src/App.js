7ceeb9a2fe58574d7d5d7063fdf3e4c40ee13b08
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-25 11:41:39 -0500

    clean up App.js and NavBar.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./App.css";
-// import axios from "axios";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Quotes from "./components/Quotes";
-import News from "./components/News";
-// import StudyGroup from "./components/StudyGroup";
 import RandomQuote from "./components/RandomQuote";
-import NewEvent from "./components/NewEvent";
-import "bootstrap/dist/css/bootstrap.min.css";
 import SavedQuotes from "./components/SavedQuotes";
-
 import { Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// import NewEvent from "./components/NewEvent";
+// import axios from "axios";
+// import News from "./components/News";
+// import StudyGroup from "./components/StudyGroup";
+
 
 function App() {
   return (
@@ -22,8 +23,8 @@ function App() {
         <Route exact path="/quotes" component={Quotes} />
         <Route exact path="/daily-quote" component={RandomQuote} />
         <Route exact path="/saved-quotes" component={SavedQuotes} />
-        <Route exact path="/news" component={News} />
-        <Route exact path="/make-study-group" component={NewEvent} />
+        {/* <Route exact path="/news" component={News} />
+        <Route exact path="/make-study-group" component={NewEvent} /> */}
       </Switch>
     </div>
   );
