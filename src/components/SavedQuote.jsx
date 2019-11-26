3df8b617f15e0131a47bf6816d5fe48a43b20ba5
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-26 16:39:09 -0500

    change props in Thumbnail2

--- src/components/SavedQuote.jsx
+++ src/components/SavedQuote.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Rating from "./Rating";
-import Thumbnail from "./Thumbnail";
+import Thumbnail2 from "./Thumbnail2";
 import Button from "./Button";
 import axios from "axios";
 
@@ -18,7 +18,10 @@ export default class SavedQuote extends Component {
       <blockquote className="blockquote text-light bg-dark p-3">
         <div className="row">
           <div className="col-sm-3 text-center">
-            <Thumbnail author={this.props.quote.author} />
+            <Thumbnail2
+              author={this.props.quote.author}
+              image={this.props.image}
+            />
           </div>
           <div className="col-sm-9">
             <p className="p-3">{this.props.quote.quote}</p>
