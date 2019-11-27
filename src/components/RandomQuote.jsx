1790182e27d6c90fec9b4952d17fcbba47dcee45
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-26 22:23:00 -0500

    add save button to randomQuote

--- src/components/RandomQuote.jsx
+++ src/components/RandomQuote.jsx
@@ -1,25 +1,33 @@
 import React, { Component } from "react";
-// import axios from "axios";
-// import Thumbnail from "./Thumbnail";
 import Rating from "./Rating";
+import Button from "./Button";
+import axios from "axios";
 
 export default class RandomQuote extends Component {
   refreshPage = () => {
     window.location.reload(false);
   };
-
+  handleSaveQuote = e => {
+    console.log("save!");
+    e.preventDefault();
+    this.setState({
+      imageUrl: `${this.props.quote.author}`,
+      quote: `${this.props.quote.en}`,
+      author: `${this.props.quote.author}`,
+      rating: `${this.props.quote.rating}`
+    });
+    const quoteData = this.state;
+    axios
+      // .post("https://ironrest.herokuapp.com/createCollection/quotes") // create a collection
+      .post("https://ironrest.herokuapp.com/quotes", quoteData) // Insert new document in collection
+      .then(e => console.log(e));
+  };
   render() {
     console.log(this.props.quote);
     return (
       <blockquote className="blockquote text-light">
         <div className="row">
           <div className="col-sm-12 text-center p-5">
-            {/* <img
-              src={require("../images/icons8-quote-left-96.png")}
-              width="100"
-              height="90"
-              alt="quote"
-            /> */}
             <button value="Refresh Page" onClick={this.refreshPage}>
               Gimme More
             </button>
@@ -31,6 +39,7 @@ export default class RandomQuote extends Component {
               {this.props.quote.author}
             </footer>
             <Rating>{this.props.quote.rating}</Rating>
+            <Button submit={e => this.handleSaveQuote(e)} info="Save" />
             {/* <span>voted by {this.state.quote.numberOfVotes} people</span> */}
           </div>
         </div>
